839452ae751237003a800b093a7418f9be805e87
Unai Lería Fortea <unicelularuni15@gmail.com>
2022-12-01 23:33:40 +0100

    Add enable and disable to BlitManager. Update gitignore

--- fitter/utils/blit_manager.py
+++ fitter/utils/blit_manager.py
@@ -15,30 +15,37 @@ class BlitManager:
         self.ax = app.ax
         self.canvas = app.figure.canvas
 
-        self.background = self.get_background()
-
-        self.canvas.mpl_connect('draw_event', self.on_draw)
-        # self.canvas.mpl_connect('button_press_event', self.on_button_press)
-        # self.canvas.mpl_connect('key_press_event', self.on_key_press)
-        # self.canvas.mpl_connect('button_release_event', self.on_button_release)
-        # self.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
+        self.enabled = False
+        self.background = None
+        self.draw_event_connection_id = None
         
 
     def get_background(self):
         for a in self.artists:
             a.poly.set_visible(False)
+        
+        for a in self.ax.artists:
+            self.ax.draw_artists(a)
             
-        self.ax.clear()
-        self.canvas.draw()
+        self.canvas.blit()
+
         return self.canvas.copy_from_bbox(self.ax.bbox)
     
+    def update_background(self):
+        self.background = self.get_background()
+        
+
     def draw(self):
         self.canvas.restore_region(self.background)
         
         for a in self.artists:
-            a.poly.set_visible(True)
-            a.update()
-            self.ax.draw_artist(a.poly)
+            try: # if custom object
+                a.poly.set_visible(True)
+                a.update()
+                self.ax.draw_artist(a.poly)
+            except AttributeError: # if matplotlib artists
+                a.set_visible(True)
+                self.ax.draw_artist(a)
             
         self.canvas.blit(self.ax.bbox)
         
@@ -46,29 +53,14 @@ class BlitManager:
     def on_draw(self, event):
         self.draw()
         
-if __name__=='__main__':
-    import matplotlib
-    matplotlib.use('WXAgg')
-    
-    class fi:
-        def __init__(self):
-            self.fig, self.ax = plt.subplots()
+    def enable(self):
+        if not self.enabled:
+            self.update_background()
+            self.enabled = True
+            self.draw_event_connection_id = self.canvas.mpl_connect('draw_event', self.on_draw)
             
-    f = fi()
-    bm = BlitManager(f)
-    
-    a = DragPoint(0.5,0.5, '*')
-    a.patch.set_transform(f.ax.transAxes)
-    f.ax.add_patch(a.patch)
-    am = DragPointManager(f.ax, a)
-    
-    b = DragPoint(0.3,0.3, 'o')
-    b.patch.set_transform(f.ax.transAxes)
-    f.ax.add_patch(b.patch)
-    bm= DragPointManager(f.ax, b)
-    
-    bm.artists.append(am)
-    bm.artists.append(bm)
-    
-    
-    plt.show()
+    def disable(self):
+        if self.enabled:
+            self.enabled = False
+            self.canvas.mpl_disconnect(self.draw_event_connection_id)
+            self.draw_event_connection_id = None
