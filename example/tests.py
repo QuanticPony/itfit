b9007a91db50063779b52f6f477e5be832ee011c
Unai Lería Fortea <unicelularuni15@gmail.com>
2022-12-06 23:50:34 +0100

    Update tests

---
+++ example/tests.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from fitter import Fitter
+
+def gauss(x, A, x0, sigma):
+    return A * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+
+def dataFunction(x, m,n, A, x0, sigma):
+    return m*x + n + gauss(x, A ,x0, sigma)
+
+
+def main():
+    noise =  np.random.normal(size=200)
+
+    xdata = np.arange(200)
+    ydata = dataFunction(xdata, -0.04, 5, 20, 120,15) + noise
+    
+    f = Fitter(xdata, ydata)
+    f()
+    
+if __name__=='__main__':
+    main()
