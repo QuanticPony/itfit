Fix error on gradient return

itfit/fit_functions/cosine/cosine.py
@@ -68,7 +68,7 @@ class DragCosineManager(DragPointCollection):
         dfdb = -a * x* np.sin(b*x +c)
         dfdc = -a * np.sin(b*x + c)
         dfdd = 1
-        return np.array ([dfda], [dfdb], [dfdc], [dfdd])
+        return np.array ([[dfda], [dfdb], [dfdc], [dfdd]])
     
     def __init__(self, dragpoints: list[DragPoint], blit_manager: BlitManager):
         """Cosine function between multiple DragPoints. Updates with them.

itfit/fit_functions/exponential/exponential.py
@@ -59,7 +59,7 @@ class DragExponentialManager(DragPointCollection):
         dfda = np.exp(b*x)
         dfdb = a * x * np.exp(b * x) 
 
-        return np.array( [dfda], [dfdb])
+        return np.array([[dfda], [dfdb]])
     
     @staticmethod
     def get_args_length():

itfit/fit_functions/gaussian/gaussian.py
@@ -62,9 +62,9 @@ class DragGaussianManager(DragPointCollection):
         """
         dfdA = np.exp(- 0.5 * (x - m)**2 / s**2)
         dfdm = A / s**2 * (x -m) * np.exp(- 0.5 * (x - m)**2 / s**2)
-        dfds = A / s**3 * (x -m)^2 * np.exp(- 0.5 * (x - m)**2 / s**2)
+        dfds = A / s**3 * (x -m)**2 * np.exp(- 0.5 * (x - m)**2 / s**2)
 
-        return np.array( [dfdA], [dfdm], [dfds] )
+        return np.array([[dfdA], [dfdm], [dfds]])
     
     @staticmethod
     def get_args_length():

itfit/fit_functions/linear/linear.py
@@ -58,7 +58,7 @@ class DragLineManager(DragPointCollection):
         """
         dfdm = x 
         dfdn = 1
-        return np.array( [[dfdm], [dfdn]])
+        return np.array([[dfdm], [dfdn]])
     
     @staticmethod
     def get_args_length():

itfit/fit_functions/quadratic/quadratic.py
@@ -63,7 +63,7 @@ class DragQuadraticManager(DragPointCollection):
         dfda = x**2
         dfdb = x
         dfdc = 1
-        return np.array([dfda], [dfdb], [dfdc])
+        return np.array([[dfda], [dfdb], [dfdc]])
     
     @staticmethod
     def get_args_length():

itfit/fit_functions/sine/sine.py
@@ -68,7 +68,7 @@ class DragSineManager(DragPointCollection):
         dfdb = a * x* np.cos(b*x +c)
         dfdc = a * np.cos(b*x + c)
         dfdd = 1
-        return np.array ([dfda], [dfdb], [dfdc], [dfdd])
+        return np.array([[dfda], [dfdb], [dfdc], [dfdd]])
     
     def __init__(self, dragpoints: list[DragPoint], blit_manager: BlitManager):
         """Sine function between multiple DragPoints. Updates with them.